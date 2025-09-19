59966025c020dbe2c9e5057f715a15b6bf818c6b
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-09-19 17:04:53 +0600

    Use localStorage for handeling cartData

--- src/context/productContext.jsx
+++ src/context/productContext.jsx
@@ -1,12 +1,21 @@
 import { createContext, useState } from "react";
+import { getCartDataFromStorage, setCartDataToStorage } from "../utils/utils";
 
 export const ProductContext = createContext();
 
 export function ProductProvider({ children }) {
-    const [cartData, setCartData] = useState([]);
+    const [cartData, setCartData] = useState(getCartDataFromStorage());
+    console.log(cartData);
+
+    // Always keep cartData updated
+    const updateCartData = (newCartData) => {
+        setCartData(newCartData);
+        setCartDataToStorage(newCartData);
+    }
+    
 
     return (
-        <ProductContext.Provider value={{ cartData, setCartData }}>
+        <ProductContext.Provider value={{ cartData, setCartData: updateCartData }}>
             {children}
         </ProductContext.Provider>
     )
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -8,11 +8,15 @@ export function getRatingImageUrl(name) {
         import.meta.url).href
 };
 
-// export function getIconImageUrl(name) {
-//     return new URL(`../assets/images/icons/${name}`,
-//         import.meta.url).href
-// }
-
 export function formatCurrency(priceCents) {
     return (Math.round(priceCents) / 100).toFixed(2);
+};
+
+export function setCartDataToStorage(cartData) {
+    localStorage.setItem("cartData", JSON.stringify(cartData));
+};
+
+export function getCartDataFromStorage() {
+    const storedCart = localStorage.getItem("cartData");
+    return storedCart ? JSON.parse(storedCart) : [];
 };
